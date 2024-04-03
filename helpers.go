805a3c5aabd1c089b949010778a8fe0fe24a2c79
aw17732 <timostroehlein@gmail.com>
2024-04-03 17:20:19 +0700

    fix default reviewer pattern matching

--- helpers.go
+++ helpers.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+	"path/filepath"
 	"strings"
 )
 
@@ -21,15 +23,28 @@ func getPullRequestReviewers(access_token string, base_url string, pr_target_bra
 	}
 
 	// Find default reviewers based on the target and source branch
-	var users []User
+	var exact_users []User
+	var any_users []User
 	for _, reviewer := range default_reviewers {
-		if reviewer.TargetRefMatcher.Id != "refs/heads/"+pr_target_branch && reviewer.TargetRefMatcher.Id != "ANY_REF_MATCHER_ID" {
+		source_branch_match, err := filepath.Match(reviewer.SourceRefMatcher.Id, pr_source_branch)
+		target_branch_match, err := filepath.Match(reviewer.TargetRefMatcher.Id, "refs/heads/"+pr_target_branch)
+		if err != nil {
+			fmt.Println("Could not match default reviewer pattern", err)
 			continue
 		}
-		if reviewer.SourceRefMatcher.Id == "refs/heads/"+pr_source_branch || reviewer.SourceRefMatcher.Id == "ANY_REF_MATCHER_ID" {
-			users = reviewer.Reviewers
+		if source_branch_match && target_branch_match {
+			exact_users = reviewer.Reviewers
+			break
+		} else if (source_branch_match || reviewer.SourceRefMatcher.Id == "ANY_REF_MATCHER_ID") && (target_branch_match && reviewer.TargetRefMatcher.Id == "ANY_REF_MATCHER_ID") {
+			any_users = reviewer.Reviewers
 			break
 		}
+	}
+	var users []User
+	if len(exact_users) > 0 {
+		users = exact_users
+	} else {
+		users = any_users
 	}
 	reviewers := make([]Reviewer, len(users))
 	for i, user := range users {
